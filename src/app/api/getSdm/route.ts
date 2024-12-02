b8f7d88d994e38856347d36c39df12f94000228b
syahrilr <syahrilramadhan754@gmail.com>
2024-12-02 17:14:33 +0800

    fix dynamic

--- src/app/api/getSdm/route.ts
+++ src/app/api/getSdm/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { NextResponse } from "next/server";
 import prismadb from "../../../../lib/prismadb";
 import { verifyJwt } from "../../../../lib/jwt";
