49907adeaa3670f8b0f508a070f4867090c98eba
syahrilr <syahrilramadhan754@gmail.com>
2024-12-02 23:25:06 +0800

    fix error next-config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { resolve } from 'path';
+
 const nextConfig = {
-    cacheHandler: process.env.NODE_ENV === 'production' ? require.resolve('./cache-handler.mjs') : undefined,
+    cacheHandler: process.env.NODE_ENV === 'production' ? resolve('./cache-handler.mjs') : undefined,
 };
 
 export default nextConfig;
