1aeea3213ee1388011313feac8cc0c91128390a9
syahrilr <syahrilramadhan754@gmail.com>
2024-12-02 18:37:31 +0800

    redis fix

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,23 +1,3 @@
-import { Redis } from '@upstash/redis';
+import { Redis } from "@upstash/redis";
 
-const getRedisUrl = () => {
-    if (process.env.KV_REST_API_URL) {
-        return process.env.KV_REST_API_URL;
-    }
-
-    throw new Error('KV_REST_API_URL is not defined');
-}
-
-const getRedisToken = () => {
-    if (process.env.KV_REST_API_TOKEN) {
-        return process.env.KV_REST_API_TOKEN;
-    }
-
-    throw new Error('KV_REST_API_TOKEN is not defined');
-}
-
-// Initialize Redis client with the correct config
-export const redis = new Redis({
-    url: getRedisUrl(),         // Pass the Redis URL
-    token: getRedisToken(),     // Pass the Redis token (if required)
-});
+export const redis = Redis.fromEnv();
